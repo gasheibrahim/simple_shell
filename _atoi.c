d8c30a253b9734f26c737f6011b07f2c29b00232
yasoug <ougouisyassine@gmail.com>
2023-05-24 14:39:51 -0700

    update atoi function

--- _atoi.c
+++ _atoi.c
@@ -9,26 +9,14 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int flag = 0;
-	int sign = 0;
-	int digit = 0;
-	int num = 0;
+	unsigned int num = 0;
 
-	while (i < _strlen(s) && flag != 1)
+	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
-			sign++;
-
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (sign % 2 != 0)
-				digit = -digit;
-			num = num * 10 + digit;
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				flag = 1;
-		}
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + (s[i] - '0');
+		if (s[i] > '9' || s[i] < '0')
+			return (-1);
 		i++;
 	}
 	return (num);
